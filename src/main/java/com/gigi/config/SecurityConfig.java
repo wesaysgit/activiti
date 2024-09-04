8b3f685f5de2249620aea679ced3cd072d9b2c51
xugan <xugan@192.168.0.103>
2024-09-04 23:27:22 +0800

    add UserDetailsService

---
+++ src/main/java/com/gigi/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.gigi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
+    }
+}
